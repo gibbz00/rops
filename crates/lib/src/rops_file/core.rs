ab6c6b997828e235e8947e037699239c5845f945
gibbz00 <gabrielhansson00@gmail.com>
2023-12-16 15:26:22 +0100

    Unit test encrypted ROPS file serialization.

--- crates/lib/src/rops_file/core.rs
+++ crates/lib/src/rops_file/core.rs
@@ -68,5 +68,20 @@ mod tests {
         fn deserializes_decrypted_rops_file() {
             FileFormatTestUtils::assert_deserialization::<YamlFileFormat, RopsFile<Decrypted, YamlFileFormat>>()
         }
+
+        #[cfg(feature = "aes-gcm")]
+        mod aes_gcm {
+            use super::*;
+
+            #[test]
+            fn serializes_encrypted_rops_file() {
+                FileFormatTestUtils::assert_serialization::<YamlFileFormat, RopsFile<Encrypted<AES256GCM>, YamlFileFormat>>()
+            }
+
+            #[test]
+            fn deserializes_encrypted_rops_file() {
+                FileFormatTestUtils::assert_deserialization::<YamlFileFormat, RopsFile<Encrypted<AES256GCM>, YamlFileFormat>>()
+            }
+        }
     }
 }
--- crates/lib/src/rops_file/map.rs
+++ crates/lib/src/rops_file/map.rs
@@ -52,7 +52,30 @@ mod mock {
             }
         }
 
-        impl MockTestUtil for RopsFileMap<Decrypted, YamlFileFormat> {
+        #[cfg(feature = "aes-gcm")]
+        impl MockFileFormatUtil<YamlFileFormat> for RopsFileMap<Encrypted<AES256GCM>, YamlFileFormat> {
+            fn mock_format_display() -> String {
+                indoc::indoc! {"
+                    hello: ENC[AES256_GCM,data:3S1E9am/,iv:WUQoQTrRXw/tUgwpmSG69xWtd5dVMfe8qUly1VB8ucM=,tag:nQUDkuh0OR1cjR5hGC5jOw==,type:str]
+                    nested_map:
+                      null_key: null
+                      array:
+                      - ENC[AES256_GCM,data:ANbeNrGp,iv:PRWGCPdOttPr5dlzT9te7WWCZ90J7+CvfY1vp60aADM=,tag:PvSLx4pLT5zRKOU0df8Xlg==,type:str]
+                      - nested_map_in_array:
+                          integer: ENC[AES256_GCM,data:qTW5qw==,iv:ugMxvR8YPwDgn2MbBpDX0lpCqzJY3GerhbA5jEKUbwE=,tag:d8utfA76C4XPzJyDfgE4Pw==,type:int]
+                      - float: ENC[AES256_GCM,data:/MTg0fCennyN8g==,iv:+/8+Ljm+cls7BbDYZnlg6NVFkrkw4GkEfWU2aGW57qE=,tag:26uMp2JmVAckySIaL2BLCg==,type:float]
+                    booleans:
+                    - ENC[AES256_GCM,data:bCdz2A==,iv:8kD+h1jClyVHBj9o2WZuAkjk+uD6A2lgNpcGljpQEhk=,tag:u3/fktl5HfFrVLERVvLRGw==,type:bool]
+                    - ENC[AES256_GCM,data:SgBh7wY=,iv:0s9Q9pQWbsZm2yHsmFalCzX0IqNb6ZqeY6QQYCWc+qU=,tag:OZb76BWCKbDLbcil4c8fYA==,type:bool]"
+                }
+                .to_string()
+            }
+        }
+
+        impl<S: RopsFileState> MockTestUtil for RopsFileMap<S, YamlFileFormat>
+        where
+            Self: MockFileFormatUtil<YamlFileFormat>,
+        {
             fn mock() -> Self {
                 serde_yaml::from_str(&Self::mock_format_display()).expect("mock yaml string not serializable")
             }
