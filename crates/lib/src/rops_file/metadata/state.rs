7f96cf88f05c72db44bf6d0ab301c345a98e3c28
gibbz00 <gabrielhansson00@gmail.com>
2023-12-22 14:37:28 +0100

    Extract `metadata::state` to its own file.

---
+++ crates/lib/src/rops_file/metadata/state.rs
@@ -0,0 +1,32 @@
+use std::{
+    fmt::{Debug, Display},
+    marker::PhantomData,
+    str::FromStr,
+};
+
+use crate::*;
+
+pub trait RopsMetadataState {
+    type Mac: Debug + PartialEq + FromStr + Display;
+}
+
+#[derive(Debug)]
+pub struct EncryptedMetadata<C: Cipher, H: Hasher>(PhantomData<C>, PhantomData<H>);
+impl<C: Cipher, H: Hasher> RopsMetadataState for EncryptedMetadata<C, H> {
+    type Mac = EncryptedMac<C, H>;
+}
+
+#[derive(Debug)]
+pub struct DecryptedMetadata<H: Hasher>(PhantomData<H>);
+
+impl<H: Hasher> RopsMetadataState for DecryptedMetadata<H> {
+    type Mac = Mac<H>;
+}
+
+mod private {
+    use super::*;
+
+    pub trait SealedRopsMetadataState {}
+    impl<C: Cipher, H: Hasher> SealedRopsMetadataState for EncryptedMetadata<C, H> {}
+    impl<H: Hasher> SealedRopsMetadataState for DecryptedMetadata<H> {}
+}
