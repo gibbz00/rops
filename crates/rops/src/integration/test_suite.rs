4a1852c7531e56dbb4e399510bb9ebb0f8821e6a
gibbz00 <gabrielhansson00@gmail.com>
2023-12-30 18:46:55 +0100

    Inline integration test suite assertions.

--- crates/rops/src/integration/test_suite.rs
+++ crates/rops/src/integration/test_suite.rs
@@ -2,22 +2,37 @@ macro_rules! generate_integration_test_suite {
     ($integration:tt) => {
         #[test]
         fn parses_private_key() {
-            <$integration as IntegrationTestUtils>::assert_parses_private_key()
+            $integration::parse_private_key($integration::mock_private_key_str()).unwrap();
         }
 
         #[test]
         fn parses_key_id() {
-            <$integration as IntegrationTestUtils>::assert_parses_key_id()
+            $integration::parse_key_id($integration::mock_key_id_str().as_ref()).unwrap();
         }
 
         #[test]
         fn encrypts_data_key() {
-            <$integration as IntegrationTestUtils>::assert_encrypts_data_key()
+            $integration::set_mock_private_key_env_var();
+
+            let expected_data_key = DataKey::mock();
+            let encrypted_data_key = $integration::encrypt_data_key(&$integration::mock_key_id(), &expected_data_key).unwrap();
+            let found_data_key = $integration::decrypt_data_key(&$integration::mock_key_id(), &encrypted_data_key)
+                .unwrap()
+                .unwrap();
+
+            assert_eq!(expected_data_key, found_data_key);
         }
 
         #[test]
         fn decrypts_data_key() {
-            <$integration as IntegrationTestUtils>::assert_decrypts_data_key()
+            $integration::set_mock_private_key_env_var();
+
+            assert_eq!(
+                DataKey::mock(),
+                $integration::decrypt_data_key(&$integration::mock_key_id(), $integration::mock_encrypted_data_key_str())
+                    .unwrap()
+                    .unwrap()
+            );
         }
 
         #[test]
--- crates/rops/src/integration/test_utils.rs
+++ crates/rops/src/integration/test_utils.rs
@@ -1,50 +1,22 @@
 use crate::*;
 
 pub trait IntegrationTestUtils: Integration {
+    fn mock_key_id_str() -> impl AsRef<str>;
+
+    fn mock_private_key_str() -> impl AsRef<str>;
+
+    fn mock_encrypted_data_key_str() -> &'static str;
+
     fn set_mock_private_key_env_var() {
         std::env::set_var(Self::private_key_env_var_name(), Self::mock_private_key_str().as_ref())
     }
-
-    fn mock_key_id_str() -> impl AsRef<str>;
 
     fn mock_key_id() -> Self::KeyId {
         Self::parse_key_id(Self::mock_key_id_str().as_ref()).unwrap()
     }
 
-    fn assert_parses_key_id() {
-        Self::mock_key_id();
-    }
-
-    fn mock_private_key_str() -> impl AsRef<str>;
-
     fn mock_private_key() -> Self::PrivateKey {
         Self::parse_private_key(Self::mock_private_key_str()).unwrap()
-    }
-
-    fn assert_parses_private_key() {
-        Self::mock_private_key();
-    }
-
-    fn mock_encrypted_data_key_str() -> &'static str;
-
-    fn assert_encrypts_data_key() {
-        Self::set_mock_private_key_env_var();
-
-        let expected_data_key = DataKey::mock();
-        let encrypted_data_key = Self::encrypt_data_key(&Self::mock_key_id(), &expected_data_key).unwrap();
-        let found_data_key = Self::decrypt_data_key(&Self::mock_key_id(), &encrypted_data_key).unwrap().unwrap();
-        assert_eq!(expected_data_key, found_data_key);
-    }
-
-    fn assert_decrypts_data_key() {
-        Self::set_mock_private_key_env_var();
-
-        assert_eq!(
-            DataKey::mock(),
-            Self::decrypt_data_key(&Self::mock_key_id(), Self::mock_encrypted_data_key_str())
-                .unwrap()
-                .unwrap()
-        )
     }
 }
 
