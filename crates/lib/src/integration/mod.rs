e94db1dbd3e068f13470ef9cbaa9cfd9a3fe630e
gibbz00 <gabrielhansson00@gmail.com>
2024-02-11 13:31:25 +0100

    Amend unused import warning not present when running tests fix.

--- crates/lib/src/integration/mod.rs
+++ crates/lib/src/integration/mod.rs
@@ -7,7 +7,7 @@ pub(crate) use error::{IntegrationError, IntegrationResult};
 
 #[cfg(feature = "age")]
 mod age;
-#[cfg(all(test, feature = "age"))]
+#[cfg(all(feature = "test-utils", feature = "age"))]
 pub(crate) use age::AgeConfig;
 #[cfg(feature = "age")]
 pub use age::AgeIntegration;
